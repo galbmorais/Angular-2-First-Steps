ea5d544198a642c7580d743cb974dd4a209e3665
gabrielluiz.morais@hotmail.com <gabrielluiz.morais@hotmail.com>
2018-09-13 13:51:04 -0300

    Two way DataBind

--- app/contatos/contato-detalhe.component.ts
+++ app/contatos/contato-detalhe.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from  '@angular/router';
+import { Location } from '@angular/common';
+import { ContatoService } from './contato.service';
+import { Contato } from './contato.model';
 
 @Component({
     moduleId: module.id,
@@ -6,6 +10,39 @@ import { Component } from '@angular/core';
     templateUrl: 'contato-detalhe.component.html'
 })
 
-export class ContatoDetalheComponent {
+export class ContatoDetalheComponent implements OnInit{
+
+    contato: Contato;
+
+    constructor(
+        private contatoService: ContatoService,
+        private route: ActivatedRoute,
+        private location: Location,
+    ){}
+    
+    ngOnInit(): void {
+
+        this.contato = new Contato (0, '', '', '');
+        //console.log("OnInit");
+
+        this.route.params.forEach((params: Params) => {
+            //Utiliza-se o +params para garantir que venha somente o numero da string
+            let id: number = +params ['id'];
+            //console.log(id);
+
+            if(id) {
+                this.contatoService.getContato(id)
+                .then((contato : Contato) => {
+                    console.log(contato);
+                    this.contato = contato;
+                });
+            }
+
+        });
+    }
+
+    log(): void {
+        console.log(this.contato);
+    }
 
 }
